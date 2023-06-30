ad6ce44851efd0b5eb64a038b3c9efbfee3cb0cb
cagox <cagox@cagox.net>
2023-06-30 05:44:18 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,6 @@
+module ocfsocial
+
+require (
+	github.com/cagox/config v0.0.0-20211105162423-8d428bff3d2b
+)
+go 1.18
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cagox/config"
+)
+
+var Config *config.ConfigurationStruct
+
+func main() {
+	fmt.Println("Hello World!")
+
+}
